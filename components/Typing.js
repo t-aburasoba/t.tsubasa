5f0589e53604ba64d7a378e3cf1b0e7cca7e0366
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-04-19 21:31:54 +0900

    modify typo

--- components/Typing.js
+++ components/Typing.js
@@ -3,7 +3,7 @@ import {Box, Text} from "@chakra-ui/react";
 
 export const Typing = () => {
     const {text} = useTypewriter({
-        words: ['Hello !', "I'm T.tsubasa.", 'Software engineer in Japan, like PHP(Laravel), JavaScript(React)', 'I like to go abroad and coding.'],
+        words: ['Hello !', "I'm T.tsubasa.", 'Software engineer in Japan, like PHP(Laravel), JavaScript(React)', 'I like going abroad and coding.'],
         loop: 10,
     })
     return(
